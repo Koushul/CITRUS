fixed preprocessing normalization

biomodels.py
@@ -72,20 +72,20 @@ class BioCitrus(nn.Module):
       self.nclusters = sga_ppi_mask.shape[1]
       self.cancer = args.cancer_type
             
-
       self.to(device)      
+      
+      
+      
       ## Simple Layers
       self.sga_layer = nn.Sequential(
         MaskedBioLayer(sga_ppi_mask, bias=enable_bias, init_weights=sga_ppi_weights),
         nn.Tanh(),
-        nn.Dropout(p=self.dropout_rate)
       )
 
 
       self.tf_layer = nn.Sequential(
         MaskedBioLayer(ppi_tf_mask, bias=enable_bias, init_weights=ppi_tf_weights),
         nn.Tanh(),
-        nn.Dropout(p=self.dropout_rate)
       )
 
       self.gep_output_layer = nn.Linear(
@@ -104,11 +104,6 @@ class BioCitrus(nn.Module):
       )
       # register a hook with the mask value
       self.gep_output_layer.weight.register_hook(lambda grad: grad.mul_(mask_value))
-
-
-      self.extra_layer = nn.Linear(self.gep_size, self.gep_size)
-
-
 
       self.optimizer = optim.Adam(
           self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay
@@ -131,7 +126,6 @@ class BioCitrus(nn.Module):
       ppi = self.sga_layer(sga)
       tf = self.tf_layer(ppi)      
       gexp = self.gep_output_layer(tf)
-      gexp = self.extra_layer(nn.Tanh()(gexp))
       
       if with_tf:
         return tf, gexp
@@ -198,10 +192,10 @@ class BioCitrus(nn.Module):
         loss.backward()
         self.optimizer.step()
 
-        # if self.constrain:
-        self.sga_layer.apply(constraints)
-        self.tf_layer.apply(constraints)
-        self.gep_output_layer.apply(constraints)
+        if self.constrain:
+          self.sga_layer.apply(constraints)
+          self.tf_layer.apply(constraints)
+          self.gep_output_layer.apply(constraints)
 
         if not self.verbose: pbar.update()
 

utils.py
@@ -629,22 +629,21 @@ class Data:
 
         # assert all(self.gep_sga.index == self.sga_sga.index)
         # assert all(self.cancerType_sga.index == self.sga_sga.index)
-        self.gep_sga = (self.gep_sga - self.gep_sga.mean(0)) / self.gep_sga.std(0)
         
         self.alterations = np.array(self.sga_sga.columns, dtype=str)
         self.tumor_ids = np.array(self.sga_sga.index, dtype=str)
         self.sga_genes = np.unique(np.array([name.split('_')[1] for name in self.alterations], dtype=str))
-        self.sga_genes.sort()
+        # self.sga_genes.sort()
         self.cancer_types = np.array(self.cancerType_sga['type'].values, dtype=str) #non-unique list
         
         ## reformat df into dual input per gene
-        _dual_alterations = np.array([('SM_'+i, 'SCNA_'+i) for i in self.sga_genes]).reshape(-1)
-        df = pd.DataFrame(columns=_dual_alterations)
-        df = self.sga_sga.reindex(columns=set(_dual_alterations).union(set(self.sga_sga.columns))).fillna(0)
-        df = df.astype(int)
-        df = df[_dual_alterations]
+        # _dual_alterations = np.array([('SM_'+i, 'SCNA_'+i) for i in self.sga_genes]).reshape(-1)
+        # df = pd.DataFrame(columns=_dual_alterations)
+        # df = self.sga_sga.reindex(columns=set(_dual_alterations).union(set(self.sga_sga.columns))).fillna(0)
+        # df = df.astype(int)
+        # df = df[_dual_alterations]
         
-        self.
+        # self.sga_sga = df
 
 
 