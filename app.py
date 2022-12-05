107462da637a62ba6e7e896e62e0bb0a030f9298
Koushul <koushulramjattun@gmail.com>
2022-12-05 10:53:28 -0500

    added spearmanr

--- app.py
+++ app.py
@@ -361,12 +361,16 @@ st.dataframe(hallmark[['Description', 'pvalue', 'qvalues', 'p.adjust']])
 results['desc'] = results.index
 
 import plotly.express as px
+from scipy.stats import spearmanr
 
 st.markdown('#### CITRUS+ versus MCF10A data')
 st.caption('Hover on data to see pathway names')
 with st.spinner('Plotting pvalues...'):
 
-    fig = px.scatter(results, x='-log10 (MCF10A pvalue)', y='-log10 (CITRUS+ pvalue)', hover_data=['desc'])
+    fig = px.scatter(results, title=f'spearmanr: {spearmanr(p_predicted, p_exp)}',
+        x='-log10 (MCF10A pvalue)', 
+        y='-log10 (CITRUS+ pvalue)', hover_data=['desc'])
+    
     st.plotly_chart(fig)
     
 with st.expander('Raw values'):
